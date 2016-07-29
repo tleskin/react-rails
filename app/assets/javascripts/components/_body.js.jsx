8dd2f6f0a4fbf5d36d11a3b26ed7323d8e727a71
Tom Leskin <tleskin@gmail.com>
2016-07-29 16:27:39 -0400

    Add ability to edit and update skills

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -29,12 +29,32 @@ let Body = React.createClass({
     let newState = this.state.skills.concat(skill);
     this.setState({ skills: newState })
   },
+  
+  handleUpdate(skill) {
+    $.ajax({
+      url: `/api/v1/skills/${skill.id}`,
+      type: 'PUT',
+      data: { skill: skill },
+      success: () => {
+        this.updateSkills(skill);
+      }
+    });
+  },
+  
+  updateSkills(skill) {
+    let skills = this.state.skills.filter((s) => { return s.id != skill.id });
+    skills.push(skill);
+
+    this.setState({ skills: skills });
+  },
 
   render() {
     return (
       <div>
         <NewSkill handleSubmit={this.handleSubmit} />
-        <AllSkills skills={this.state.skills} handleDelete={this.handleDelete} />
+        <AllSkills skills={this.state.skills} 
+                   handleDelete={this.handleDelete}
+                   handleUpdate={this.handleUpdate} />
       </div>
     )
   }
